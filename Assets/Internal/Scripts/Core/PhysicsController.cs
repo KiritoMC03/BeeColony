8fa33f0cefc05e62cadb322907ac1680c2a427b2
KiritoMC <prog.me.2017@gmail.com>
2021-06-03 17:11:21 +0600

    Version 2.2! - physics optimization;

---
+++ Assets/Internal/Scripts/Core/PhysicsController.cs
@@ -0,0 +1,9 @@
+﻿using Utils;
+
+namespace Internal.Scripts.Core
+{
+    public class PhysicsController : MonoBehaviourBase
+    {
+        
+    }
+}
---
+++ Assets/Internal/Scripts/Core/Spawners/ResourceSourcesSpawner.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace BeeColony.Core.Spawners
+{
+    public class ResourceSpawner : MonoBehaviourBase
+    {
+        [SerializeField] private ResourceSource
+    }
+}
