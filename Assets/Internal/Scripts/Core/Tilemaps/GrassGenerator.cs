8139515d4e2bf423354e1ee18893a83af507ad48
KiritoMC <prog.me.2017@gmail.com>
2021-06-08 18:45:19 +0600

    Version 3.1! - added a grass tile generator; - 4 options for grass tiles;

---
+++ Assets/Internal/Scripts/Core/Tilemaps/GrassGenerator.cs
@@ -0,0 +1,36 @@
+﻿using BeeColony.Core;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Utils;
+using Random = UnityEngine.Random;
+
+namespace Internal.Scripts.Core.Tilemaps
+{
+    public class GrassGenerator : MonoBehaviourBase
+    {
+        [SerializeField] private Tilemap tilemap;
+        [SerializeField] private Tile[] grassPrefabs;
+        [SerializeField] private float spawnRadius = 40f;
+        [SerializeField] private uint count = 200;
+        
+        private void Start()
+        {
+            Generate();
+        }
+
+        private void Generate()
+        {
+            var position = new Vector3Int();
+            for (int i = 0; i < count; i++)
+            {
+                position = new Vector3Int(
+                    (int)Random.Range(-spawnRadius, spawnRadius),
+                    (int)Random.Range(-spawnRadius, spawnRadius),
+                    0);
+                
+                tilemap.SetTile(position, grassPrefabs[Random.Range(0, grassPrefabs.Length - 1)]);
+            }
+        }
+    }
+}
